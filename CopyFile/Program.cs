238ae20acc8c1c23dbeb7ada12e0ced2cf19d3c0
wangrui <wryl@163.com>
2023-12-11 15:09:17 +0800

    添加项目文件。

---
+++ CopyFile/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// 从命令行参数中获取源目录和目标目录，如果没有提供则使用默认值
+string source = args.Length > 0 ? args[0] : @"C:\Users\user\source";
+string target = args.Length > 1 ? args[1] : @"C:\Users\user\target";
+// 遍历源目录下的所有文件，包括子目录中的文件
+foreach (string file in Directory.GetFiles(source, "*", SearchOption.AllDirectories))
+{
+    // 获取文件大小，单位为字节
+    long size = new FileInfo(file).Length;
+    // 判断文件大小是否在指定范围内，并重命名文件
+    if (size > 5120 && size < 204800)
+    {
+        File.Copy(file, Path.Combine(target, "profile.sav"), true);
+    }
+    else if (size > 307200)
+    {
+        File.Copy(file, Path.Combine(target, "save_0.sav"), true);
+    }
+}
+Console.WriteLine("Success!");
+Console.ReadKey();
---
+++ run.bat
@@ -0,0 +1,8 @@
+@echo off
+rem 设置你的程序的路径，可以根据需要修改
+set program=CopyFile.exe
+rem 设置你的程序的两个参数，可以根据需要修改
+set param1=C:\Users\ray\AppData\Local\Packages\PerfectWorldEntertainment.GFREMP2_jrajkyc4tsa6w\SystemAppData\wgs\000901FF1122CFBD_00000000000000000000000076F212E3
+set param2=D:\bd\Wingdk
+rem 运行你的程序并传递参数
+%program% %param1% %param2%
