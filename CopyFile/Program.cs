d3a534d3479163336d1f7346a213e8bca54fe3d1
wangrui <wryl@163.com>
2023-12-18 18:06:53 +0800

    增加异常捕获

--- CopyFile/Program.cs
+++ CopyFile/Program.cs
@@ -3,20 +3,33 @@
 // 从命令行参数中获取源目录和目标目录，如果没有提供则使用默认值
 string source = args.Length > 0 ? args[0] : @"C:\Users\user\source";
 string target = args.Length > 1 ? args[1] : @"C:\Users\user\target";
-// 遍历源目录下的所有文件，包括子目录中的文件
-foreach (string file in Directory.GetFiles(source, "*", SearchOption.AllDirectories))
+try
 {
-    // 获取文件大小，单位为字节
-    long size = new FileInfo(file).Length;
-    // 判断文件大小是否在指定范围内，并重命名文件
-    if (size > 5120 && size < 94800)
+    int profilenum = 0;
+    int savenum = 0;
+    // 遍历源目录下的所有文件，包括子目录中的文件
+    foreach (string file in Directory.GetFiles(source, "*", SearchOption.AllDirectories))
     {
-        File.Copy(file, Path.Combine(target, "profile.sav"), true);
+        // 获取文件大小，单位为字节
+        long size = new FileInfo(file).Length;
+        // 判断文件大小是否在指定范围内，并重命名文件
+        if (size > 5120 && size < 94800)
+        {
+            File.Copy(file, Path.Combine(target, "profile.sav"), true);
+            profilenum++;
+        }
+        else if (size > 97200)
+        {
+            File.Copy(file, Path.Combine(target, "save_0.sav"), true);
+            savenum++;
+        }
     }
-    else if (size > 97200)
-    {
-        File.Copy(file, Path.Combine(target, "save_0.sav"), true);
-    }
+    Console.WriteLine("Success!");
+    Console.WriteLine($"profilenum:{profilenum},savenum:{savenum} all of them must be 1");
+    Console.ReadKey();
 }
-Console.WriteLine("Success!");
-Console.ReadKey();
+catch (Exception e)
+{
+    Console.WriteLine(e.ToString());
+    Console.ReadKey();
+}
